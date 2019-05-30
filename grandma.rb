e4998b9a9eec9906499ed175ab15175165218161
tommaso auerbach <tsa302@nyu.edu>
2019-05-30 19:56:52 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,10 @@
-# Write a speak_to_grandma method.
+# grandma = "deaf"
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+speak_to_grandma = "answer"
+if "answer" == downcase
+puts "HUH?! SPEAK UP, SONNY!"
+elsif "answer" == upcase
+  puts "NO, NOT SINCE 1938!"
+elsif "answer" == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+end
